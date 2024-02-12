667882b59ed29394af77049784388b72475d5b01
Geraldo Silva <geraldojuni@hotmail.com>
2024-02-12 20:07:57 -0300

    chore: configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Main } from './views/Main/Main';
 
 function App() {
   return (
     <>
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<Main />} />
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
